ef9d436d826c4034dcfe5436ec06dbfb57a21e5a
Dan Arrington <dan.arrington@gmail.com>
2014-06-13 07:49:15 -0700

    Show tee time if player has not started today yet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def score_or_time(score)
+    if score.today == '-'
+      case Time.now.wday
+        when 4
+          score.first_round
+        when 5
+          score.second_round
+        when 6
+          score.third_round
+        when 7
+          score.fourth_round
+        end
+    else
+      score.today
+    end
+  end
 end

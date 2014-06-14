0c0965fb42586e14fa9d2c9ab43ff29aad9ac91e
Dan Arrington <dan.arrington@gmail.com>
2014-06-13 17:47:22 -0700

    Handle inconsistant data for players who miss cut (pt 1)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,6 +18,11 @@ module ApplicationHelper
   end
 
   def edt_to_local t
+    begin
     ActiveSupport::TimeZone.new('America/New_York').parse(t).in_time_zone('Pacific Time (US & Canada)').strftime('%l:%M %P')
+    rescue
+      t
+    end
+
   end
 end

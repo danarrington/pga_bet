2da97c9696512a8769fd4a62c99c5a513cefaf97
Dan Arrington <dan.arrington@gmail.com>
2014-07-15 18:41:24 -0700

    Added more player results properties

--- app/models/player_results.rb
+++ app/models/player_results.rb
@@ -1,8 +1,10 @@
 class PlayerResults
-  attr_accessor :today, :name
+  attr_accessor :today, :name, :thru, :total
 
   def initialize(score_data)
     @today = score_data.today.to_i
     @name = score_data.name
+    @thru = score_data.thru.to_i
+    @total = score_data.total.to_i
   end
 end
--- spec/models/player_results_spec.rb
+++ spec/models/player_results_spec.rb
@@ -5,20 +5,28 @@ describe PlayerResults do
     context 'loading Ben Crane' do
       # leaberboard.first
       # <Hashie::Mash first_round="63" fourth_round="48" name="Ben Crane" position="1" second_round="65" strokes="-11" third_round="69" thru="12" today="+2" total="-11">
+      subject(:result) {PlayerResults.new(@leaderboard.first)}
       before :each do
         f = File.open(Rails.root.join 'spec/data/4th_round_midday.txt')
         @leaderboard = Marshal.load(f)
         f.close
       end
       it 'should set the today property' do
-        results = PlayerResults.new(@leaderboard.first)
-        expect(results.today).to eq 2
+        expect(result.today).to eq 2
       end
 
       it 'should set the name property' do
-        results = PlayerResults.new(@leaderboard.first)
-        expect(results.name).to eq "Ben Crane"
+        expect(result.name).to eq "Ben Crane"
       end
+
+      it 'should set the thru property' do
+        expect(result.thru).to eq 12
+      end
+
+      it 'should set the total property' do
+        expect(result.total).to eq -11
+      end
+
     end
   end
 end

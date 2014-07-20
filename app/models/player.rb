1183398ce7fa9441b58f1eafa69afd7c04e37210
Dan Arrington <dan.arrington@gmail.com>
2014-07-20 09:28:46 -0700

    Fix sunday scores to keep

--- app/models/player.rb
+++ app/models/player.rb
@@ -10,7 +10,7 @@ class Player < ActiveRecord::Base
 
 
   def self.scores_to_keep
-    [6, 7].include?(Time.zone.now.wday)  ? 2 :4
+    [6, 0].include?(Time.zone.now.wday)  ? 2 :4
   end
 
   def self.calculate_total_score(players)

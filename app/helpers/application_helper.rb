463c5702cf309158a526689a9edae171cc73c12d
Dan Arrington <dan.arrington@gmail.com>
2015-06-21 10:29:48 -0700

    Remove unused helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,4 @@
 module ApplicationHelper
-
-  def edt_to_local t
-    begin
-    ActiveSupport::TimeZone.new('America/New_York').parse(t).in_time_zone('Pacific Time (US & Canada)').strftime('%l:%M %P')
-    rescue
-      t
-    end
-
-  end
-
   def used_class(score)
     !!score.used ? 'is-used' : 'is-dropped'
   end
